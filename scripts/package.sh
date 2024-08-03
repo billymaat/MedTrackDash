a47eb54755f24320fafa0b1d1dc6560ef44940f6
Billy Maat <bsmaat@gmail.com>
2024-08-03 22:18:18 +0100

    moved files to api folder

--- scripts/package.sh
+++ scripts/package.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # package the solution
-tar -czvf medtrackdash_api.tar.gz -C MedTrackDash/bin/Release/net8.0/ .
+tar -czvf medtrackdash_api.tar.gz -C api/MedTrackDash/bin/Release/net8.0/ .
